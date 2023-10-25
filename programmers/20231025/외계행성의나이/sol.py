19373cd757e7984dbfcb2bd02c34ecf5cf6aee75
wonyoung94 <jun2young94@gmail.com>
2023-10-25 21:01:52 +0900

    update

--- programmers/20231025/외계행성의나이/sol.py
+++ programmers/20231025/외계행성의나이/sol.py
@@ -39,3 +39,18 @@ print(solution(100)) # baa
 
 # 실제 내가 실행한 코드를 강제로 미친듯이 짧게 하면 이렇게 됨...
 # solution=lambda age:str(age).translate(str.maketrans('0123456789','abcdefghij'))
+
+
+# 한솔이 답변
+# 알파벳 정렬하는 함수 이용하기 위해 import 
+# from string import ascii_lowercase
+
+# def solution(age):
+#     # 알파벳 리스트 만들고 그 인덱스에 해당하는 값 문자열형태로 더하기
+#     answer = ''
+#     alpha = list(ascii_lowercase)
+
+#     age = str(age)
+#     for char in age:
+#         answer += alpha[int(char)]
+#     return answer
