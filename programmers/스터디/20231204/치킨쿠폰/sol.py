635a611c4f43c1b510c96ba63ace75caf1f0ee53
wonyoung94 <jun2young94@gmail.com>
2023-12-04 20:52:18 +0900

    study

--- programmers/스터디/20231204/치킨쿠폰/sol.py
+++ programmers/스터디/20231204/치킨쿠폰/sol.py
@@ -27,7 +27,7 @@ print(solution(1999)) #222
 # 가지고 있는 쿠폰이 총 10장이므로 서비스 치킨 1마리를 추가로 주문할 수 있습니다.
 # 따라서 108 + 10 + 1 + 1 = 120 을 return합니다.
 
-#뭐야 이거..
+# 뭐야 이거..
 # def solution(chicken):
 #     return int(chicken*0.11111111111)
 
@@ -35,3 +35,33 @@ print(solution(1999)) #222
 # def solution(chicken):
 #     answer = (max(chicken,1)-1)//9
 #     return answer
+
+# 민정이와의 고통파티
+# def solution(chicken):
+#     coupon = 0
+#     service_chicken = 0
+#     left_coupon = 0
+#     sum_coupon = []
+#     s_c = 0
+#     answer = []
+#     while chicken > 0:
+#         coupon = chicken
+#         service_chicken = chicken // 10
+        
+#         left_coupon = coupon - (service_chicken*10)
+#         sum_coupon.append(left_coupon)
+#         chicken = service_chicken
+        
+#         answer.append(service_chicken)
+        
+#         s_c += left_coupon
+        
+#     while s_c >= 10:
+#         service_chicken= s_c // 10
+#         answer.append(service_chicken)
+#         s_c = s_c - (service_chicken *10)
+#         s_c += service_chicken
+
+#     return sum(answer)
+
+# print(solution(1999)) #222
